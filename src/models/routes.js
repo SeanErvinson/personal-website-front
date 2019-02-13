199ca2c411d2824686ed9502ba3d47f7d6b7b6be
SeanErvinson <ervinsonong@gmail.com>
2019-02-13 22:52:14 +0800

    added error route and page

--- src/models/routes.js
+++ src/models/routes.js
@@ -2,10 +2,12 @@ import Home from '../pages/Home.vue'
 import Projects from '../pages/Projects.vue'
 import About from '../pages/About'
 import Contact from '../pages/Contact.vue'
+import Error from '../pages/Error.vue'
 
 export const routes = [
     { name: 'home', path: '/', component: Home, display: 'Home'},
     { name: 'about', path: '/about', component: About, display: 'About'},
     { name: 'projects', path: '/projects', component: Projects, display: 'Projects' },
-    { name: 'crojects', path: '/contact', component: Contact, display: 'Contact' }
+    { name: 'crojects', path: '/contact', component: Contact, display: 'Contact' },
+    { name: '*', path: '*', component: Error, display: '404 Error' }
 ];
