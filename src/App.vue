5c464291ad9e8561dde33696391def961295ef92
SeanErvinson <ervinsonong@gmail.com>
2019-02-14 14:34:39 +0800

    ,adfed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/projects">Projects</router-link> |
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/projects">Projects</router-link>|
       <router-link to="/contact">Contact</router-link>
     </div>
     <router-view/>
@@ -11,4 +11,20 @@
 </template>
 
 <style>
+/* @import '/assets/css/fonts.css'; */
+/* @import '../node_modules/bootstrap/scss/bootstrap.scss'; */
+
+html {
+  background-color: white;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+* {
+    box-sizing: border-box;
+}
+a {
+  font-family: Nunito-Light;
+}
+
 </style>
