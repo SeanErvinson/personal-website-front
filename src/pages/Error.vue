946528f7394b601a94b01a00216d3c8a87384866
SeanErvinson <ervinsonong@gmail.com>
2019-02-16 21:18:26 +0800

    Added Error page

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -1,13 +1,38 @@
 <template>
-
+  <div class="row h-100 align-items-center">
+    <div class="col-6 mx-auto">
+      <h1 class="error header">Oh no (|:^O</h1>
+      <div class="error-description">
+        <p>This page cannot be found | Error 404</p>
+        <p>Here, let me help you get back.</p>
+        <router-link class="return-home" to="/">CANDY</router-link>
+      </div>
+      <div class="pt-3">
+        <h6>
+          <strong>Fun Fact:</strong> I'm really bad at html, css, and front-end stuff and design in general.
+        </h6>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.error {
+  font-family: "Nunito-Bold";
+  color: #ff5146;
+}
+.error-description {
+  font-size: 1.4em;
+}
+.return-home {
+  color: #0078d7;
+}
+.description,
+.error-description {
+  font-family: "Didact Gothic-Regular";
+}
 </style>
