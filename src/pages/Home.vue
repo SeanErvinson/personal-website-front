efb4d259797ee77046f94d73bbbcf9100d8da46a
SeanErvinson <ervinsonong@gmail.com>
2019-02-16 21:18:32 +0800

    added home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,14 @@
 <template>
+    <div class="row h-100 justify-content-center align-items-center">
+        <h2 class="header">Welcome</h2>
+    </div>
 </template>
 
 <script>
 
 export default {
+
 }
 </script>
+<style scoped>
+</style>
