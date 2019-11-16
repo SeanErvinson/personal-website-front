4ee56bbe28b5c0791ca787d54f3ebd673d794d20
Sean Ervinson <ervinsonong@gmail.com>
2019-11-16 11:04:57 +0800

    Created navbar component

---
+++ src/components/ui/NavBar/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav id="menu">
+    <NavMenu></NavMenu>
+  </nav>
+</template>
+
+<script>
+import NavMenu from "@/components/NavMenu.vue";
+export default {
+  components: { NavMenu }
+};
+</script>
+
+<style scoped>
+nav {
+  margin: 1rem 1.5rem;
+  letter-spacing: 8px;
+  text-align: right;
+  position: absolute;
+  right: 0%;
+}
+/* #menu {
+  margin-right: 30px;
+  letter-spacing: 8px;
+  padding-top: 20px;
+  text-align: right;
+}
+#menu li a {
+  display: inline-block;
+  color: #0864c7;
+}
+.nav-item {
+  text-align: right;
+}
+@media only screen and (max-width: 768px) {
+  #menu {
+    padding-bottom: 40px;
+    padding-top: 20px;
+    margin-right: 20px;
+  }
+} */
+</style>
