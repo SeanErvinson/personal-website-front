25ed3413347587a9eb02a9924b6bacfd1393901e
Sean Ervinson <ervinsonong@gmail.com>
2019-11-16 22:46:23 +0800

    Update about content

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,40 +1,48 @@
 <template>
-  <div class="row h-100 align-items-center">
-    <div class="col-10 mx-auto">
-      <h2 class="header">About Me</h2>
-      <div class="description justify-content-center">
-        <p>Hello there! :^)</p>
-        <p>
-          I started programming
-          <span class="odometer odometer-theme-default years"></span>
-          {{yearText}} and
-          <span class="odometer odometer-theme-default days"></span>
-          {{dayText}} ago;
-          this
-          was roughly around the time I started college. I would consider myself as a self-taught programmer even though
-          I am currently taking up Information Technology (My school
-          <s>sucks</s> is meh).
-        </p>
-        <p>I do a lot of C# stuff, and I occasionally try android development and game development.</p>
-        <p>
-          During my free time, I love exploring new skills such as electronics, server-related,
-          machine learning and stuff that interest me.
-        </p>
-        <p>
-          If you're interested feel free to check out my
-          <router-link to="/projects">stuff </router-link>or
-          contact me at
-          <a :href="'mailto:' + emailLink">{{emailLink}}</a>.
-        </p>
-      </div>
-      <h4 class="header">Elsewhere</h4>
-      <ul id="about-social-links">
-        <li v-for="(media, index) in links" :key="index" class="about-social-icon">
-          <social-link :url="media.url" :name="media.name" :src="media.images.png" />
-        </li>
-      </ul>
+  <section id="about">
+    <h2 class="header">About Me</h2>
+    <div class="description justify-content-center">
+      <p>Hello there! :^)</p>
+      <p>
+        I started programming
+        <span class="odometer odometer-theme-default years"></span>
+        {{yearText}} and
+        <span class="odometer odometer-theme-default days"></span>
+        {{dayText}} ago;
+        this
+        was roughly around the time I started college. I would consider myself as a self-taught programmer even though
+        I am currently taking up Information Technology (My school
+        <s>sucks</s> is meh).
+      </p>
+      <p>
+        I do a lot of C# stuff, but recently I've been using
+        <a
+          target="_blank"
+          href="https://www.python.org/"
+        >Python</a> for pretty much everything (Dynamic typing is amazing), and on some occasion I do mobile development using
+        <a
+          target="_blank"
+          href="https://flutter.dev/"
+        >Flutter</a>.
+      </p>
+      <p>
+        During my free time, I love exploring new skills such as electronics, server-related,
+        machine learning and stuff that interest me.
+      </p>
+      <p>
+        If you're interested feel free to check out my
+        <router-link to="/projects">stuff</router-link>&nbsp;or
+        contact me at
+        <a :href="'mailto:' + emailLink">{{emailLink}}</a>.
+      </p>
     </div>
-  </div>
+    <h4 class="header">Elsewhere</h4>
+    <ul id="about-social-links">
+      <li v-for="(media, index) in links" :key="index" class="about-social-icon">
+        <social-link :url="media.url" :name="media.name" :src="media.images.png" />
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
@@ -131,8 +139,5 @@ export default {
 .description {
   font-size: 1.1em;
   letter-spacing: 0.1em;
-}
-ul {
-  list-style-type: none;
 }
 </style>
