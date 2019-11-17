8cf3d71a1da0f7b629e2817be97e99eb2f898cba
Sean Ervinson <ervinsonong@gmail.com>
2019-11-17 19:35:06 +0800

    Replace bootstrap table with normal

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,101 +1,91 @@
 <template>
-  <div class="row h-100 align-items-center">
-    <div class="col-10 mx-auto">
-      <h2 class="header">My Projects</h2>
-      <b-table responsive :borderless="true" :striped="true" :fields="fields" :items="items">
-        <template slot="github" slot-scope="data">
-          <template v-if="data.value">
-            <a :href="data.value" target="_blank">
-              <simple-svg
-                class="icon highlight"
-                :filepath="circle"
-                :width="'24px'"
-                :height="'24px'"
-              />
+  <section id="projects">
+    <h2 class="header">My Projects</h2>
+    <table class="project-table">
+      <thead>
+        <th>Title</th>
+        <th>
+          <Icon viewBox="0 0 24 24" class="icon">
+            <IconGithub />
+          </Icon>
+        </th>
+        <th>
+          <Icon viewBox="0 0 24 24" class="icon">
+            <IconLink />
+          </Icon>
+        </th>
+        <th>
+          <Icon viewBox="0 0 24 24" class="icon">
+            <IconPlaystore />
+          </Icon>
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.title">
+          <td>{{item.title}}</td>
+          <td>
+            <a :href="item.github" target="_blank">
+              <Icon
+                :iconName="item.title + ' Github'"
+                viewBox="0 0 24 24"
+                class="icon action blue-highlight"
+                :class="{disabled: item.github === null}"
+              >
+                <IconCircle />
+              </Icon>
             </a>
-          </template>
-          <template v-else>
-            <simple-svg class="icon disabled" :filepath="circle" :width="'24px'" :height="'24px'" />
-          </template>
-        </template>
-        <template slot="link" slot-scope="data">
-          <template v-if="data.value">
-            <a :href="data.value" target="_blank">
-              <simple-svg
-                class="icon highlight"
-                :filepath="square"
-                :width="'24px'"
-                :height="'24px'"
-              />
+          </td>
+          <td>
+            <a :href="item.link" target="_blank">
+              <Icon
+                :iconName="item.title + ' Link'"
+                viewBox="0 0 24 24"
+                class="icon action blue-highlight"
+                :class="{disabled: item.link === null}"
+              >
+                <IconSquare />
+              </Icon>
             </a>
-          </template>
-          <template v-else>
-            <simple-svg class="icon disabled" :filepath="square" :width="'24px'" :height="'24px'" />
-          </template>
-        </template>
-        <template slot="playStore" slot-scope="data">
-          <template v-if="data.value">
-            <a :href="data.value" target="_blank">
-              <simple-svg
-                class="icon highlight"
-                :filepath="triangle"
-                :width="'24px'"
-                :height="'24px'"
-              />
+          </td>
+          <td>
+            <a :href="item.playStore" target="_blank">
+              <Icon
+                :iconName="item.title + ' PlayStore'"
+                viewBox="0 0 24 24"
+                class="icon action blue-highlight"
+                :class="{disabled: item.playStore === null}"
+              >
+                <IconTriangle />
+              </Icon>
             </a>
-          </template>
-          <template v-else>
-            <simple-svg
-              class="icon disabled"
-              :filepath="triangle"
-              :width="'24px'"
-              :height="'24px'"
-            />
-          </template>
-        </template>
-
-        <template slot="HEAD_github" slot-scope="data">
-          <simple-svg class="icon" :filepath="data.label" :width="'24px'" :height="'24px'" />
-        </template>
-        <template slot="HEAD_link" slot-scope="data">
-          <simple-svg class="icon" :filepath="data.label" :width="'24px'" :height="'24px'" />
-        </template>
-        <template slot="HEAD_playStore" slot-scope="data">
-          <simple-svg class="icon" :filepath="data.label" :width="'24px'" :height="'24px'" />
-        </template>
-      </b-table>
-      <p>Note: Website is under construction. API endpoint currently not working</p>
-    </div>
-  </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
-import { SimpleSVG } from "vue-simple-svg";
+import Icon from "@/components/shared/Icon";
+import IconCircle from "@/components/shared/Icon/icons/IconCircle";
+import IconSquare from "@/components/shared/Icon/icons/IconSquare";
+import IconTriangle from "@/components/shared/Icon/icons/IconTriangle";
+import IconGithub from "@/components/shared/Icon/icons/IconGithub";
+import IconLink from "@/components/shared/Icon/icons/IconLink";
+import IconPlaystore from "@/components/shared/Icon/icons/IconPlaystore";
+
 export default {
-  components: { "simple-svg": SimpleSVG },
+  components: {
+    Icon,
+    IconCircle,
+    IconSquare,
+    IconTriangle,
+    IconGithub,
+    IconLink,
+    IconPlaystore
+  },
   data() {
     return {
-      triangle: require("../assets/images/triangle.svg"),
-      square: require("../assets/images/square.svg"),
-      circle: require("../assets/images/circle.svg"),
-      fields: {
-        title: {
-          label: "Title",
-          sortable: true
-        },
-        github: {
-          label: require("../assets/images/github.svg"),
-          tdClass: "table-links-data"
-        },
-        link: {
-          label: require("../assets/images/link.svg"),
-          tdClass: "table-links-data"
-        },
-        playStore: {
-          label: require("../assets/images/playstore.svg"),
-          tdClass: "table-links-data"
-        }
-      },
       items: [
         {
           title: "TextLint",
@@ -124,16 +114,35 @@ export default {
         },
         {
           title: "interrogate.me",
-          github: "https://github.com/SeanErvinson/interrogate-me-Website",
+          github: "https://github.com/SeanErvinson/interrogate-me-web",
           link: null,
           playStore: null
         }
       ]
     };
-  },
-  methods: {}
+  }
 };
 </script>
 
 <style scoped>
+#projects {
+  width: 90%;
+}
+.project-table {
+  width: 100%;
+  text-align: center;
+}
+
+.project-table thead th:first-child,
+.project-table tbody td:first-child {
+  text-align: left;
+}
+
+.project-table tbody tr:nth-of-type(odd) {
+  background-color: rgba(0, 0, 0, 0.02);
+}
+.project-table td,
+.project-table th {
+  padding: 4px;
+}
 </style>
