7cfbddc153eadf66d364f8e5c9d528d78685821c
Sean Ervinson <ervinsonong@gmail.com>
2019-11-17 20:24:54 +0800

    Added block to menu items

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,29 +1,32 @@
 <template>
-    <ul>
-        <li v-for="(route, index) in routes" :key="index">
-            <router-link v-if="!route.hidden" :to="route.path" class="menu-item" >
-                {{route.display}}
-            </router-link>
-        </li>
-    </ul>
+  <ul>
+    <li v-for="(route, index) in routes" :key="index">
+      <router-link v-if="!route.hidden" :to="route.path" class="menu-item">{{route.display}}</router-link>
+    </li>
+  </ul>
 </template>
 <script>
 import { routes } from "../models/routes";
 
 export default {
-    data(){
-        return{
-            routes,
-        }
-    }
-}
+  data() {
+    return {
+      routes
+    };
+  }
+};
 </script>
 <style scoped>
-.menu-item.router-link-exact-active{
-    background: rgb(230, 230, 230);
+.menu-item.router-link-exact-active {
+  background: rgb(230, 230, 230);
 }
-.menu-item.router-link-exact-active::after{
-    content: "|";
-    background: #0864c7;
+.menu-item.router-link-exact-active::after {
+  content: "|";
+  background: #0864c7;
+}
+@media screen and (min-width: 991px) {
+  .menu-item {
+    display: block;
+  }
 }
 </style>
