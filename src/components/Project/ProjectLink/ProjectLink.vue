d1d9c197e87efa28de75d1d5ec744d5b3254dfc6
Sean Ervinson <ervinsonong@gmail.com>
2020-01-16 23:58:06 +0800

    Added project link

---
+++ src/components/Project/ProjectLink/ProjectLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="link-container">
+    <a :href="url" target="_blank">
+      <slot />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String
+  }
+};
+</script>
+<style lang="scss" scoped>
+.link-container {
+  & + & {
+    margin-left: $xxsmall;
+  }
+}
+</style>
