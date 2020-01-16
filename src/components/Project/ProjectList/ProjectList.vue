ae3d74853948ffc8e360fa276b5c93eefc887b3d
Sean Ervinson <ervinsonong@gmail.com>
2020-01-16 23:29:54 +0800

    Added project list

---
+++ src/components/Project/ProjectList/ProjectList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="project-list">
+    <ProjectTile v-for="project in projects" :key="project.id" :project="project"></ProjectTile>
+  </div>
+</template>
+
+<script>
+import ProjectTile from "@/components/Project/ProjectTile";
+export default {
+  props: {
+    projects: Object
+  },
+  components: {
+    ProjectTile
+  }
+};
+</script>
+<style lang="scss" scoped>
+.project-list {
+  display: grid;
+  column-gap: $small;
+  row-gap: $small;
+  grid-template-columns: 1fr 1fr;
+}
+
+@media (max-width: $mediaBp2Width) {
+  .project-list {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
