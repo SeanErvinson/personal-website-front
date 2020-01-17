fcaf5e36f56444f798903e86e027e663bd7d2c7d
Sean Ervinson <ervinsonong@gmail.com>
2020-01-17 23:17:23 +0800

    Replaced static data with json

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,52 +1,47 @@
 <template>
   <section id="projects">
     <h2 class="header">My Projects</h2>
-    <ProjectList :projects="items"></ProjectList>
+    <ProjectList :projects="projects"></ProjectList>
   </section>
 </template>
 
 <script>
 import ProjectList from "@/components/Project/ProjectList";
+import { projectService } from "@/actions";
 
 export default {
+  beforeMount() {
+    this._fetchProjects();
+  },
   components: {
     ProjectList
   },
+  methods: {
+    _fetchProjects() {
+      projectService.get("/").then(response => {
+        this.next = response.data.next;
+        this.previous = response.data.previous;
+        response.data.results.map(item => {
+          this.projects.push({
+            title: item.title,
+            slug: item.slug,
+            github: item.github_url,
+            playstore: item.playstore_url,
+            site: item.site_url,
+            tags: item.tags,
+            created_on: item.created_on
+          });
+        });
+        this.count = response.data.count;
+      });
+    }
+  },
   data() {
     return {
-      items: [
-        {
-          title: "TextLint",
-          github: "https://github.com/SeanErvinson/text-lint-web",
-          site: "http://app.seanervinson.com/textlint/",
-          playstore: null
-        },
-        {
-          title: "NuWo - Number to Word Converter",
-          github: "https://github.com/SeanErvinson/NumbersToWords",
-          site: null,
-          playstore: null
-        },
-        {
-          title: "Protect It",
-          github: null,
-          site: null,
-          playstore:
-            "https://play.google.com/store/apps/details?id=com.SheepLabs.ProtectIt"
-        },
-        {
-          title: "seanervinson.com",
-          github: null,
-          site: "https://www.seanervinson.com",
-          playstore: null
-        },
-        {
-          title: "interrogate.me",
-          github: "https://github.com/SeanErvinson/interrogate-me-web",
-          site: null,
-          playstore: null
-        }
-      ]
+      next: "",
+      previous: "",
+      count: 0,
+      projects: []
     };
   }
 };
