ce1dd15dabb4ed289272921a5a0e42b25177a660
Sean Ervinson <ervinsonong@gmail.com>
2020-01-18 01:24:37 +0800

    Remove bootstrap classes and tweaked ui

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -1,39 +1,38 @@
 <template>
-  <div class="row h-100 align-items-center">
-    <div class="col-6 mx-auto">
-      <h1 class="error header">Oh no (|:^O</h1>
-      <div class="error-description">
-        <p>This page cannot be found | Error 404</p>
-        <p>Here, let me help you get back.</p>
-        <router-link class="return-home" to="/">CANDY</router-link>
-      </div>
-      <div class="pt-3">
-        <h6>
-          <strong>Fun Fact:</strong> I'm really bad at html, css, and front-end stuff and design in general.
-        </h6>
-      </div>
+  <section id="error">
+    <h1>Oh no (|:^O</h1>
+    <div class="description">
+      <p>This page cannot be found | Error 404</p>
+      <p>Here, let me help you get back.</p>
+      <router-link class="return-home" to="/">CANDY</router-link>
     </div>
-  </div>
+    <h6>
+      <strong>Fun Fact:</strong> I'm really bad at html, css, and front-end stuff and design in general.
+    </h6>
+  </section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped>
-.error {
-  font-family: 'Nunito', sans-serif;
-  font-weight: bold;
-  color: #ff5146;
-}
-.error-description {
-  font-size: 1.4em;
-}
-.return-home {
-  color: #0078d7;
-}
-.description,
-.error-description {
-  font-family: "Didact Gothic";
+<style lang="scss" scoped>
+#error {
+  text-align: center;
+  h1 {
+    font-family: $secondary-font, sans-serif;
+    font-weight: bold;
+    color: $error-color;
+  }
+  .description {
+    font-family: $primary-font;
+    p {
+      font-size: $smedium;
+    }
+    .return-home {
+      color: $success-color;
+      font-size: $mlarge;
+    }
+  }
 }
 </style>
