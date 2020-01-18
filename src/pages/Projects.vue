1dc77af469b49d8053d932418f0d5b49197d6faf
Sean Ervinson <ervinsonong@gmail.com>
2020-01-18 22:34:20 +0800

    Added error message when api returns an error

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,6 +1,7 @@
 <template>
   <section id="projects">
     <h1>My Projects</h1>
+    <h4 v-if="errorMessage.length > 1">{{errorMessage}}</h4>
     <ProjectList :projects="projects"></ProjectList>
   </section>
 </template>
@@ -18,22 +19,28 @@ export default {
   },
   methods: {
     _fetchProjects() {
-      projectService.get("/").then(response => {
-        this.next = response.data.next;
-        this.previous = response.data.previous;
-        response.data.results.map(item => {
-          this.projects.push({
-            title: item.title,
-            slug: item.slug,
-            github: item.github_url,
-            playstore: item.playstore_url,
-            site: item.site_url,
-            tags: item.tags,
-            created_on: item.created_on
+      projectService
+        .get("/")
+        .then(response => {
+          this.next = response.data.next;
+          this.previous = response.data.previous;
+          response.data.results.map(item => {
+            this.projects.push({
+              title: item.title,
+              slug: item.slug,
+              github: item.github_url,
+              playstore: item.playstore_url,
+              site: item.site_url,
+              tags: item.tags,
+              created_on: item.created_on
+            });
           });
+          this.count = response.data.count;
+        })
+        .catch(error => {
+          this.errorMessage =
+            "Uh oh! There seems to be a problem with the server.";
         });
-        this.count = response.data.count;
-      });
     }
   },
   data() {
@@ -41,7 +48,8 @@ export default {
       next: "",
       previous: "",
       count: 0,
-      projects: []
+      projects: [],
+      errorMessage: ""
     };
   }
 };
