23f5da1ecdf71c4de5afa22bbd5ebf6b893a8e00
Sean Ervinson <ervinsonong@gmail.com>
2020-01-26 22:38:35 +0800

    Implemented ui for privacy policy

---
+++ src/components/Project/ProjectPrivacyPolicy/ProjectPrivacyPolicy.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="privacy-policy" v-html="content"></div>
+</template>
+
+<script>
+import { projectService } from "@/actions";
+export default {
+  data() {
+    return {
+      content: ""
+    };
+  },
+  beforeMount() {
+    this._fetchPrivacyPolicy();
+  },
+  methods: {
+    _fetchPrivacyPolicy() {
+      return projectService
+        .get(`${this.$route.params.slug}`)
+        .then(response => (this.content = response.data.privacy_policy))
+        .catch(message => null);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#privacy-policy {
+  padding: $medium 0;
+}
+</style>
