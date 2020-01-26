a401be7812766f41dd01c1fb6f49280ee8cdb3ef
Sean Ervinson <ervinsonong@gmail.com>
2020-01-26 22:37:31 +0800

    Added routes for project detail and privacy policy

--- src/models/routes.js
+++ src/models/routes.js
@@ -1,13 +1,17 @@
-import Home from '@/pages/Home.vue'
-import Projects from '@/pages/Projects.vue'
+import Home from '@/pages/Home'
+import Projects from '@/pages/Projects'
+import ProjectDetail from '@/components/Project/ProjectDetail'
+import ProjectPrivacyPolicy from '@/components/Project/ProjectPrivacyPolicy'
 import About from '@/pages/About'
-import Contact from '@/pages/Contact.vue'
-import Error from '@/pages/Error.vue'
+import Contact from '@/pages/Contact'
+import Error from '@/pages/Error'
 
 export const routes = [
     { name: 'home', path: '/', component: Home, display: 'Home' },
     { name: 'about', path: '/about', component: About, display: 'About' },
     { name: 'projects', path: '/projects', component: Projects, display: 'Projects' },
-    { name: 'contact', path: '/contact', component: Contact, display: 'Contact'},
+    { name: 'projectDetail', path: '/projects/:slug', component: ProjectDetail },
+    { name: 'projectPrivacyPolicy', path: '/projects/:slug/privacy-policy', component: ProjectPrivacyPolicy },
+    { name: 'contact', path: '/contact', component: Contact, display: 'Contact' },
     { name: 'error', path: '*', component: Error, display: 'Error', hidden: true },
 ];
