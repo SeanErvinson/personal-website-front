b2267e32f001b185806095026bc976ab1350015e
Sean Ervinson <ervinsonong@gmail.com>
2020-02-03 20:33:25 +0800

    BASE url changes depending on environment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const BASE_URL = "https://seanervinson.com/api/v1/"
-const BASE_URL = "http://localhost:8000/api/v1/"
+const BASE_URL = process.env.VUE_APP_API_URL;
 
 export const projectService = axios.create({
     baseURL: BASE_URL + "projects/",
